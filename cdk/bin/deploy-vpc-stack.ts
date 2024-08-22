c0a9c570e16dab632dd65d3012bb32afcb15eba9
briankim02121997 <briankim02121997@gmail.com>
2024-08-22 19:58:14 +0700

    fix(cdk): put construct into stacks

--- cdk/bin/deploy-vpc-stack.ts
+++ cdk/bin/deploy-vpc-stack.ts
@@ -1,22 +1,32 @@
 #!/usr/bin/env node
 import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
 import { VpcConstruct } from "@constructs";
 
-const app = new cdk.App();
+class VpcStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
 
-new VpcConstruct(app, "VpcConstruct", {
-  maxAzs: 3,
-  subnetConfiguration: [
-    {
-      name: "Public",
-      subnetType: cdk.aws_ec2.SubnetType.PUBLIC,
-    },
-    {
-      name: "Private",
-      subnetType: cdk.aws_ec2.SubnetType.PRIVATE_WITH_EGRESS,
-    },
-  ],
-});
+    new VpcConstruct(this, "VpcConstruct", {
+      maxAzs: 3, // Specify the number of availability zones
+      subnetConfiguration: [
+        {
+          cidrMask: 24, // Define the size of the subnet
+          name: "Public",
+          subnetType: cdk.aws_ec2.SubnetType.PUBLIC,
+        },
+        {
+          cidrMask: 24,
+          name: "Private",
+          subnetType: cdk.aws_ec2.SubnetType.PRIVATE_WITH_EGRESS,
+        },
+      ],
+    });
+  }
+}
+
+const app = new cdk.App();
+new VpcStack(app, "VpcStack");
 
 app.synth();
